ebcff6276e8d0bd56cb05309bdb14ed69da1fdbd
ptr-stasiv <160587121+ptrptrstasiv@users.noreply.github.com>
2024-09-17 20:02:29 +0300

    Remove xj plguin from example project

--- Examples/Showcase/Plugins/XJMusicPlugin/Source/XJMusicPlugin/Private/Engine/EngineBase.cpp
+++
@@ -1,3 +0,0 @@
-// Copyright (c) XJ Music Inc. (https://xjmusic.com) All Rights Reserved.
-
-#include "Engine/EngineBase.h"
--- Examples/Showcase/Plugins/XJMusicPlugin/Source/XJMusicPlugin/Private/Settings/XJMusicDefaultSettings.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright (c) XJ Music Inc. (https://xjmusic.com) All Rights Reserved.
-
-#include <XJMusicPlugin/Public/Settings/XJMusicDefaultSettings.h>
-#include "Engine/DataTable.h"
