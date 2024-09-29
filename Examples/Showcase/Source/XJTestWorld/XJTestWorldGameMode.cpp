d7ed8b1ed956453d369f80754cc4783181324510
ptr-stasiv <160587121+ptrptrstasiv@users.noreply.github.com>
2024-09-29 21:07:37 +0300

    Fix packaging issues

--- Examples/Showcase/Source/XJTestWorld/XJTestWorldGameMode.cpp
+++ Examples/Showcase/Source/XJTestWorld/XJTestWorldGameMode.cpp
@@ -7,20 +7,5 @@
 
 AXJTestWorldGameMode::AXJTestWorldGameMode()
 {
-	// use our custom PlayerController class
-	PlayerControllerClass = AXJTestWorldPlayerController::StaticClass();
 
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
 }
